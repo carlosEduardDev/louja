de44f7c1008751dc5954a1c86434be8d4a8c0462
carlos <carlos8189.mk@gmail.com>
2024-07-18 17:02:06 -0300

    feat(header):added logic form and styles

---
+++ src/components/layout/actions/actions.tsx
@@ -0,0 +1,17 @@
+import { Heart, ShoppingBag } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const Actions = () => {
+  return (
+    <nav className="flex gap-3">
+      <Link to="/products">
+        <ShoppingBag />
+      </Link>
+      <Link to="/favorites">
+        <Heart />
+      </Link>
+    </nav>
+  );
+};
+
+export default Actions;
