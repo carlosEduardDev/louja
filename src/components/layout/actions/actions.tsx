906080b1218dbf98079a1802f1606729fa910d92
carlos <carlos8189.mk@gmail.com>
2024-07-18 18:46:38 -0300

    fix(header):changed navigation links

--- src/components/layout/actions/actions.tsx
+++ src/components/layout/actions/actions.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Actions = () => {
   return (
     <nav className="flex gap-3">
-      <Link to="/products">
+      <Link to="/shop-cart">
         <ShoppingBag />
       </Link>
       <Link to="/favorites">
