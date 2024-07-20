19b6e28d211403b8030d198b0c457abf6f686aec
carlos <carlos8189.mk@gmail.com>
2024-07-19 22:55:41 -0300

    feat(products):added page fetch products

--- src/components/layout/skeleton-products/skeleton-products.tsx
+++ src/components/layout/skeleton-products/skeleton-products.tsx
@@ -2,13 +2,12 @@ import { Skeleton } from "../../ui/skeleton";
 
 const SkeletonProducts = () => {
   return (
-    <section className="py-6 lg:px-28 md:px-18 sm:px-8 px-4 grid gap-8 grid-cols-[repeat(auto-fill,minmax(200px,1fr))] ">
-      {Array.from({ length: 42 }, () => 1).map((index) => (
-        <div key={index} className="flex flex-col space-y-3 py-8">
-          <Skeleton className="h-[125px] w-[] rounded-xl" />
+    <section className="py-6 lg:px-28 md:px-18 sm:px-8 px-4 grid gap-8 grid-cols-[repeat(auto-fill,minmax(290px,1fr))] ">
+      {Array.from({ length: 42 }, () => 1).map(() => (
+        <div key={crypto.randomUUID()} className="flex flex-col space-y-3 py-8">
+          <Skeleton className="h-[380px] rounded-xl" />
           <div className="space-y-2">
-            <Skeleton className="h-4 w-[200px]" />
-            <Skeleton className="h-4 w-[200px]" />
+            <Skeleton className="h-6" />
           </div>
         </div>
       ))}
