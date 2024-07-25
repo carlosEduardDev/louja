f29df362b09752c17e98eee6303838b425e49f5a
carlos <carlos8189.mk@gmail.com>
2024-07-25 00:02:37 -0300

    feat(store):added context

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./globals.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router.tsx";
+import { GlobalStorage } from "./store/globalStorage.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <GlobalStorage>
+      <RouterProvider router={router} />
+    </GlobalStorage>
   </React.StrictMode>
 );
