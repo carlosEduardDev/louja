99ccccec81c112f8568e63f9fc3fb9ab91ec109e
carlos <carlos8189.mk@gmail.com>
2024-07-28 16:24:36 -0300

    feat(footer): added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom"
 import Header from "./components/layout/header"
+import Footer from "./components/layout/footer/footer"
 
 const App = () => {
   return (
     <main>
       <Header />
       <Outlet />
+      <Footer />
     </main>
   )
 }
---
+++ src/components/layout/footer/footer.tsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <section className="bg-primary text-background text-center py-8">
+      <p className="text-xl mb-2">Louja&reg; - Alguns direitos reservados</p>
+
+      <p className="text-lg">
+        Feito por Carlos Eduado -{" "}
+        <a
+          href="https://github.com/carlosEduardDev/louja"
+          className="underline"
+          target="_blank"
+        >
+          Visite o repositório
+        </a>
+      </p>
+    </section>
+  );
+};
+
+export default Footer;
