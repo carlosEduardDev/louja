d5c8be03674a0334937b38c712691ab37e5e3ee2
carlos <carlos8189.mk@gmail.com>
2024-07-28 14:12:48 -0300

    chore(seo):SEO adjusts and not found page implemented

--- src/pages/notfound/not-found.tsx
+++ src/pages/notfound/not-found.tsx
@@ -1,7 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
   return (
-    <div>Not found</div>
-  )
-}
+    <section className="lg:py-16 lg:px-28 md:px-18 sm:px-8 px-4">
+      <h1 className="text-lg mb-4">
+        A página que você tentou acessar não existe...
+      </h1>
+      <Button className="text-base">
+        <Link to="/">Voltar para o início</Link>
+      </Button>
+    </section>
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
